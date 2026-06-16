1ea9a7d231b927970a919018bd1e939fdbadd572
smfKizen <scott.filips@kizen.com>
2026-06-16 11:13:26 -0500

    ADD secrets for port and host

--- src/automationSteps/mysql/script.py
+++ src/automationSteps/mysql/script.py
@@ -5,16 +5,27 @@ def connect_to_mysql():
 
   target = None
 
-  secret_name = next(iter(key for key in secrets if key.endswith("mysql_password")), None)
   outputs.log(f'Secret: {secrets}')
+
+  secret_name = next(iter(key for key in secrets if key.endswith("mysql_password")), None)
+  
   outputs.log(f'Secret name: {secret_name}')
   MYSQL_PASSWORD = secrets[secret_name]
   outputs.log(f'MYSQL_PASSWORD: {MYSQL_PASSWORD}')
-  
-    
+
+  secret_host = next(iter(key for key in secrets if key.endswith("mysql_host")), None)
+  outputs.log(f'Secret name: {secret_host}')
+  MYSQL_HOST = secrets[secret_host]
+  outputs.log(f'MYSQL_HOST: {MYSQL_HOST}')
+
+  secret_port = next(iter(key for key in secrets if key.endswith("mysql_port")), None)
+  outputs.log(f'Secret name: {secret_port}')
+  MYSQL_PORT = secrets[secret_port]
+  outputs.log(f'MYSQL_PORT: {MYSQL_PORT}')
+
   connection_config = {
-      'host': '6.tcp.ngrok.io',
-      'port': 28010,
+      'host': MYSQL_HOST,
+      'port': int(MYSQL_PORT),
       'user': 'ScottF_Kizen',               
       'password': MYSQL_PASSWORD,
       'database': 'demodb',   
